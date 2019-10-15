3b093254b0e712b23879286cf68f0837aa9fe5af
2019201096 <kumar.pallav@students.iiit.ac.in>
2019-10-15 23:02:51 +0530

    first commit

--- server_file_v01.cpp
+++ server_file_v01.cpp
@@ -1,3 +1,4 @@
+//creating multi threading on server side
 #include <stdio.h> 
 #include <sys/socket.h> 
 #include <stdlib.h> 
@@ -6,10 +7,60 @@
 #include <iostream> 
 #include <unistd.h>
 #include <arpa/inet.h>
-
+#define NUM_THREADS 15
 using namespace std;
-int main(){
-	int sockser;
+struct taskids{
+	int socketfd;
+	int threadid;
+};
+void* handlethreadrequest(void* tasks1){
+			FILE *fp=fopen("gitpwd","rb");
+			struct taskids *tasks=(struct taskids*)tasks1;
+			int sockfd=tasks->socketfd;
+			int threadid=tasks->threadid;
+			//cout<<"thread id: "<<threadid<<endl;
+			// fseek(fp,0,SEEK_END);
+			// int size=ftell(fp);
+					//cout<<size;
+					//rewind(fp);
+					//fseek(fp,0,SEEK_SET);
+					//send(sockfd,&size,sizeof(int),0);
+
+
+				unsigned int x=threadid*50;
+					fseek(fp,x,SEEK_SET);
+					// if(fseek(fp,0,x)!=0){
+					// 	return NULL;//fseek return non zero value on failure
+					// }		
+					//cout<<"x val "<<x<<endl;	
+					int size=50;
+					char Buffer[50];
+					int n;
+					//char y=(char)x;
+					//send to send the fp position
+					//char *buf=(char*)&x;
+				//	int insize=sizeof(x);
+				//	int sent=0;
+					//int buf[1]={x};
+					//int y=buf+sent;
+					cout<<x<<endl;
+					if((n=fread(Buffer,sizeof(char),50,fp))>0 && size>0)
+					{
+						//cout<<n;
+						send(sockfd,&x,sizeof(x),0);
+						send(sockfd,Buffer,n,0);
+						//memset(Buffer,'\0',256);
+						size=size-n;
+					}
+			
+	
+	
+	//close(sockfd);
+	fclose(fp);
+	return NULL;
+}
+void server(){
+		int sockser;
 	int sockfd;
 	struct sockaddr_in server1,client;
 	unsigned int len;
@@ -24,25 +75,7 @@ int main(){
 	bzero(&server1.sin_zero,8);
 	
 	len=sizeof(server1);
-	// if(connect(socksv,(struct sockaddr*)&server,len)==-1)
-	// {
-	// 	perror("connect: ");
-	//  	exit(1);	
-	// }
-	// FILE *fp=fopen("gitpwd","r");
-	// fseek(fp,0,SEEK_END);
-	// int size=ftell(fp);
-	// rewind(fp);
-	// //send(socksv,&size,sizeof(gitpwd),0);
-	// char Buffer[256];
-	// while((n=fread(Buffer,sizeof(char),256,fp))>0 && size>0){
-	// 	send(socksv,Buffer,n,0);
-	// 	memset(Buffer,'\0',256);
-	// 	size=size-n;
-	// }
 
-	// fclose(fp);
-	// close(socksv);
 
 	if(bind(sockser,(struct sockaddr*)&server1,len)==-1){
 		perror("bind: ");
@@ -61,47 +94,43 @@ int main(){
 			exit(1);
 		}
 
-			FILE *fp=fopen("gitpwd","rb");
-	fseek(fp,0,SEEK_END);
-	int size=ftell(fp);
-	//cout<<size;
-	rewind(fp);
-	//fseek(fp,0,SEEK_SET);
-	send(sockfd,&size,sizeof(int),0);
-	char Buffer[256];
-	int n;
-	while((n=fread(Buffer,sizeof(char),256,fp))>0 && size>0){
-		//cout<<n;
-		send(sockfd,Buffer,n,0);
-		//memset(Buffer,'\0',256);
-		size=size-n;
+   FILE *fp=fopen("gitpwd","rb");
+			fseek(fp,0,SEEK_END);
+			int size=ftell(fp);
+					//cout<<size;
+					rewind(fp);
+					//fseek(fp,0,SEEK_SET);
+					send(sockfd,&size,sizeof(int),0);
+					fclose(fp);
+	pthread_t threads[NUM_THREADS]; 
+	struct taskids *task[NUM_THREADS];
+	//int *taskids[NUM_THREADS];
+	for(int t=0;t<NUM_THREADS;t++) 
+	{
+		task[t]=(taskids*)malloc(sizeof(taskids));
+	task[t]->threadid = t;
+	task[t]->socketfd =sockfd;
+	//(int *) malloc(sizeof(int));
+//	*taskids[t] = t;
+	//printf("Creating thread %d\n", t);
+	int rc = pthread_create(&threads[t], NULL, handlethreadrequest,(void *) task[t] );
+	if (rc) {
+		printf("ERROR; return code from pthread_create() is %d\n", rc); 
+		exit(-1);
+		}
 	}
-	
+	for(int t=0;t<NUM_THREADS;t++) 
+	{
+		pthread_join(threads[t],NULL);
+	}
 	close(sockfd);
-	fclose(fp);
+	
 }
 close(sockser);
-//*********************************************************
-		// FILE *fp=fopen("newf","ab");
+}
 
-		// char Buffer[256];
-		// int file_size;
-		// memset(Buffer,0,256);
-		// recv(sockfd,&file_size,sizeof(int),0);
-		// cout<<file_size<<"\n ";
-		// int n;
-		// while ((n=recv(sockfd,Buffer,25,0))>0 && file_size>0){
-		// 	fwrite(Buffer,sizeof(char),n,fp);
-		// 	//memset(Buffer,'\0',25);
-		// 	file_size=file_size-n;
-		// }
-		// close(sockfd);
-		// close(sockser);
-		// fclose(fp);
-//******************************************************************
-	// 	count=send(sockcli,str,strlen(str),0);
-	// 	cout<<"sent "<<count<<" bytes to "<<inet_ntoa(client.sin_addr)<<endl;
-	// 	close(sockcli);
-	// }
+int main(){
+
+server();
 
 }
